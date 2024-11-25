2ffce176e3e1a9a5f9ee3ab32741947b4073509b
YourNewUsername <youremail@example.com>
2024-11-25 14:31:41 +0530

    Add multiple new files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link as ScrollLink } from 'react-scroll';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from "../../assets/logoname2.png";
-// import video from "../../assets/bg3.mp4";
+import video from "../../assets/bg4.mp4";
 
 const Navbar = () => {
   useEffect(() => {
@@ -26,21 +26,32 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       {/* Uncomment if video background is needed */}
-      {/* <video
+      <video
         className="video"
         autoPlay
         muted
         loop
         playsInline
         src={video}
-      ></video> */}
+      ></video>
       <div className="navbar">
         <div className="navbar-logo">
           <img src={logo} alt="Logo" />
         </div>
         <ul className="nav-menu">
           <li><ScrollLink to="home-section" smooth={true} duration={500}>Home</ScrollLink></li>
-          <li><ScrollLink to="explore-section" smooth={true} duration={500}>Explore</ScrollLink></li>
+          <li className="dropdown">
+            <ScrollLink to="/" smooth={true} duration={500}>
+            Explore <i className="bi bi-caret-down-fill"></i>
+            </ScrollLink>
+            <ul className="dropdown-content">
+              <li><Link to="/poster-page1">Typography</Link></li>
+              <li><Link to="/poster-page">Poster</Link></li>
+              <li><Link to="/service">Branding</Link></li>
+              <li><Link to="/logo-design">illustrator</Link></li>
+              <li><Link to="/more">Adobe</Link></li>
+            </ul>
+          </li>
           <li className="dropdown">
             <ScrollLink to="services-section" smooth={true} duration={500}>
               Services <i className="bi bi-caret-down-fill"></i>
@@ -50,7 +61,7 @@ const Navbar = () => {
               <li><Link to="/poster-page">Poster Design</Link></li>
               <li><Link to="/service">App Design</Link></li>
               <li><Link to="/logo-design">UI/UX Design</Link></li>
-              <li><Link to="/more">More</Link></li>
+              <li><Link to="/more">Web Design</Link></li>
             </ul>
           </li>
           <li><ScrollLink to="portfolio-section" smooth={true} duration={500}>Work</ScrollLink></li>
@@ -61,7 +72,7 @@ const Navbar = () => {
           <input type="text" placeholder="Search..." />
         </div>
         <div className="nav-connect">
-          Connect
+          Learn
         </div>
       </div>
     </div>
